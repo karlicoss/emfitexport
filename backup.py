e17f1c601f7f699bbcf40251c7439623ba469db3
Dima Gerasimov <karlicoss@gmail.com>
2019-12-21 13:28:13 +0000

    some improvements for resilience

--- backup.py
+++ backup.py
@@ -1,124 +1,162 @@
 #!/usr/bin/env python3
-import io, re, requests, sys, time
+import io
 import json
 import logging
+import re
+import sys
+import time
 from os import listdir, rename
 from os.path import join, lexists
+from pathlib import Path
+from typing import Any, Dict, Iterator, List, Union
+
+import backoff # type: ignore
+import requests
+from kython.klogging import setup_logzero
 
 from emfit_secrets import TOKEN # type: ignore
 
-from kython import setup_logzero
 
-import backoff # type: ignore
+# ugh. it's a mess https://github.com/samuelmr/emfit-qs/pull/1/files
+QS_API = 'https://qs-api.emfit.com'
+API_QS = 'https://api-qs.emfit.com'
 
-BPATH = '/L/backups/emfit/'
+
+BPATH = Path('/L/backups/emfit/')
+
 
 def get_logger():
     return logging.getLogger('emfit-backup')
+
 
 class NoKey(Exception):
     pass
 
 @backoff.on_exception(backoff.expo, NoKey, max_time=60 * 30)
-def get_device():
-    r = get("/api/v1/user/get")
+def get_device() -> str:
+    # TODO ugh it's a mess
+    # in browser, it uses https://qs-api.emfit.com/api/v1/user and bearer: in Authorisation field??
+    r = get('/api/v1/user/get', timeout=1)
     jj = r.json()
     u = jj.get('user', None)
     if u is None:
         # sometimes it just happens for no good reason...
-        get_logger().error(f'"no user" error: {jj}')
+        get_logger().error('"no user" error: %s', jj)
         raise NoKey
     return u['devices']
+
 
 @backoff.on_exception(backoff.expo, NoKey, max_time=60 * 30)
 def get_presences(device):
     # TODO eh,is that all of them??
-    r = get("/v4/presence/{0}/latest".format(device))
+    r = get(f"/v4/presence/{device}/latest")
     jj = r.json()
     nd = jj.get('navigation_data', None)
     if nd is None:
+        get_logger().error("no navigation data: %s", jj)
         raise NoKey
     presences = [presence["id"] for presence in nd]
     # mm, date is returned in funny format, without year
     return set(presences)
 
-def download_presence(device, pres):
-    logger = get_logger()
-    attempt = 0
-    while attempt < 5:
-        js = get(f"/v4/presence/{device}/{pres}").json()
-        if 'id' not in js:
-            logger.warning(f"Bad json: {js}")
-            time.sleep(1)
-            attempt += 1
-            continue
-        return js
-    else:
-        return None
 
-def get(path):
-    r = requests.get("https://api-qs.emfit.com" + path, params = { "remember_token" : TOKEN }, verify=False)
+class Retry(Exception):
+    pass
+
+
+@backoff.on_exception(backoff.expo, Retry, max_tries=5)
+def download_presence(device, presence):
+    logger = get_logger()
+    js = get(f"/v4/presence/{device}/{presence}").json()
+    if 'id' not in js:
+        logger.warning(f"Bad json: {js}")
+        raise Retry
+    else:
+        return js
+
+
+def get(path, base=API_QS, **kwargs):
+    r = requests.get(
+        base + path,
+        params={
+            'remember_token': TOKEN,
+        }, verify=False, **kwargs)
     r.raise_for_status()
     return r
 
-def merge(records):
-    assert len(records) > 1
-    merged = records[0]
-    for record in records[1:]:
-        merged.append(record[1])
-    return merged
 
 def get_existing():
-    res = set() 
+    res = set()
     for p in listdir(BPATH):
         if p.endswith('.json'):
             res.add(p[:-len('.json')])
     return res
 
 
-def backup():
-    errors = False
-
+def fetch_presences() -> Iterator[Exception]:
     logger = get_logger()
+
     device = get_device()
+    logger.info('Fetching data for device %s', device)
     presences = get_presences(device)
-    for presence in presences:
-        ppath = join(BPATH, presence + '.json')
-        if lexists(ppath):
-            logger.info(f"Skipping {ppath}, already exists")
-            continue
-        js = download_presence(device, presence)
-        if js is None:
-            logging.error(f"Error while getting presence {presence}")
-            errors = True
+
+    for p in presences:
+        ppath = BPATH / (p + '.json')
+        if ppath.exists():
+            logger.info('skipping %s, already exists', ppath)
+            continue 
+
+        try:
+            js = download_presence(device, presence=p)
+        except Exception as e:
+            logging.error('error while getting presence %s', p)
+            logging.exception(e)
+            yield e
             continue
 
-        logger.info(f"Downloaded {presence}, saving")
-        with open(ppath, 'w') as fo:
+        logger.info('downloaded presence %s, saving', p)
+        with ppath.open('w') as fo:
             json.dump(js, fo, ensure_ascii=False, indent=2, sort_keys=True)
 
     existing = get_existing()
     # next, clean up turds
     diff = existing.difference(presences)
     if len(diff) > 5: # kinda arbitrary
-        raise RuntimeError(f'Too many differences {diff}')
+        yield RuntimeError(f'Too many differences {diff}')
 
     for d in diff:
         logger.info(f"Archiving turd {d}")
-        OLD = join(BPATH, d + ".json")
-        NEW = join(BPATH, d + ".json.old")
+        OLD = BPATH / (d + ".json")
+        NEW = BPATH / ( d + ".json.old")
         rename(OLD, NEW)
 
-    if errors:
+
+def backup():
+    logger = get_logger()
+
+    errors = list(fetch_presences())
+    if len(errors) > 0:
         sys.exit(1)
+
 
 def main():
     import urllib3 # type: ignore
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-    setup_logzero(get_logger(), level=logging.INFO)
+    setup_logzero(get_logger(), level=logging.DEBUG)
+    setup_logzero(logging.getLogger('requests'), level=logging.DEBUG)
     setup_logzero(logging.getLogger('backoff'), level=logging.DEBUG)
     backup()
 
+
 if __name__ == '__main__':
     main()
+
+
+# TODO what was that for?
+# def merge(records):
+#     assert len(records) > 1
+#     merged = records[0]
+#     for record in records[1:]:
+#         merged.append(record[1])
+#     return merged
