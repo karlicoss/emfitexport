291918b8acb5e4ccf772af52bd76688e068c53f2
Dima Gerasimov <karlicoss@gmail.com>
2022-06-06 23:56:59 +0100

    dvmstatus capture: add systemd install script

--- src/emfitexport/dvmstatus.py
+++ src/emfitexport/dvmstatus.py
@@ -12,7 +12,7 @@ import sys
 import pytz
 
 import urllib.request
-def grab(ip: str) -> None:
+def grab(ip: str) -> str:
     url = ip + '/dvmstatus.htm'
     try:
         return urllib.request.urlopen(url, timeout=1).read().decode('utf8')
@@ -40,11 +40,68 @@ def capture(*, ip: str, to: Path) -> None:
             time.sleep(1)
 
 
+### copy pasted from promnesia
+import subprocess
+from typing import List
+
+SYSTEMD_TEMPLATE = '''
+[Unit]
+Description=Emfit local capture
+
+[Install]
+WantedBy=default.target
+
+[Service]
+ExecStart={launcher} {extra_args}
+Type=simple
+Restart=always
+'''
+
+def systemd(*args, method=subprocess.check_call) -> None:
+    method([
+        'systemctl', '--no-pager', '--user', *args,
+    ])
+
+
+def _install_systemd(*, name: str, out: Path, launcher: str, largs: List[str]) -> None:
+    unit_name = name
+
+    import shlex
+    extra_args = ' '.join(shlex.quote(str(a)) for a in largs)
+
+    out.write_text(SYSTEMD_TEMPLATE.format(
+        launcher=launcher,
+        extra_args=extra_args,
+    ))
+
+    try:
+        systemd('stop' , unit_name, method=subprocess.run) # ignore errors here if it wasn't running in the first place
+        systemd('daemon-reload')
+        systemd('enable', unit_name)
+        systemd('start' , unit_name)
+        systemd('status', unit_name)
+    except Exception as e:
+        print(f"Something has gone wrong... you might want to use 'journalctl --user -u {unit_name}' to investigate", file=sys.stderr)
+        raise e
+###
+
+
 import click
 @click.command()
 @click.option('--ip', type=str, required=True)
 @click.option('--to', type=Path, required=True)
-def main(ip: str, to: Path) -> None:
+@click.option('--install-systemd', type=bool, is_flag=True, default=False)
+def main(*, ip: str, to: Path, install_systemd: bool) -> None:
+    if install_systemd:
+        name = 'emfit_capture'
+        out = Path(f'~/.config/systemd/user/{name}.service').expanduser()
+        _install_systemd(
+            name=name,
+            out=out, 
+            launcher=str(Path(__file__).absolute()),
+            largs=['--ip', ip, '--to', str(to)],
+        )
+        return
     capture(ip=ip, to=to)
 
 
